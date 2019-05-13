4624531b69410b6ee78d5d927f269849f2e7d7fa
Henry <hrmstockdale@gmail.com>
2019-05-13 11:45:22 +0100

    basic setup

---
+++ index.js
@@ -0,0 +1,12 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(port, () => {
+    console.log(`Listening on ${port}`);
+});
+
